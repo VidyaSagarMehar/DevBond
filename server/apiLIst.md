f01828faed1b6c6d2204e6dba3669de9017bf0b2
VidyaSagarMehar <vidyasagark890@gmail.com>
2025-08-28 16:24:41 +0530

    pagination added - feed route

--- server/apiLIst.md
+++ server/apiLIst.md
@@ -21,4 +21,10 @@
 - GET /user/feed - Gets you the profile of other users on platform
 
 
+-Add pagination
+- feed?page=1&Limit=10 => 1-10 => .skip(0) & .limit(10)
+- feed?page=2&Limit=10 => 11-20 => .skip(10) & .limit(10)
+- feed?page=3&Limit=10 => 21-30 => .skip(20) & .limit(10)
+  skip formula -> (page - 1)*limit
+
 STATUS : ignore | intrested | accepted | rejected
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -68,6 +68,11 @@ userRouter.get('/feed', userAuth, async (req, res) => {
 
 		const loggedInUser = req.user;
 
+		const page = parseInt(req.query.page) || 1;
+		let limit = parseInt(req.query.limit) || 10;
+		limit = limit > 50 ? 50 : limit; //setting max limit to 50
+		const skip = (page - 1) * limit;
+
 		// Find all connection request (sent + received)
 		const connectionRequests = await ConnectionRequest.find({
 			$or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -86,9 +91,12 @@ userRouter.get('/feed', userAuth, async (req, res) => {
 				{ _id: { $nin: Array.from(hideUsersFromFeed) } },
 				{ _id: { $ne: loggedInUser._id } },
 			],
-		}).select(USER_SAFE_DATA);
+		})
+			.select(USER_SAFE_DATA)
+			.skip(skip)
+			.limit(limit);
 
-		res.send(users);
+		res.json({ data: users });
 	} catch (err) {
 		res.status(400).send({
 			error: 'error getting the data',
