301f5da59ce5128bb695e2e43d7b82d8d33788e2
VidyaSagarMehar <vidyasagark890@gmail.com>
2025-09-01 13:02:35 +0530

    Frontend initial commit with router, tailwind, daisyUI

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import NavBar from './Components/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Body from './Body';
+import Login from './Components/Login';
+import Profile from './Components/Profile';
+
+function App() {
+	return (
+		<>
+			{/* <NavBar /> */}
+			<BrowserRouter basename="/">
+				<Routes>
+					<Route path="/" element={<Body />}>
+						<Route path="/login" element={<Login />} />
+						<Route path="/profile" element={<Profile />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
