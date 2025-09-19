6d37ba6120fe69c2724e6b0680c3276224729711
VidyaSagarMehar <vidyasagark890@gmail.com>
2025-09-20 00:14:10 +0530

    updated DB pw

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-	await mongoose.connect('mongodb://localhost:27017/devTinder');
+	await mongoose.connect(
+		'mongodb+srv://VidyaSagar:vidya1234@cluster0.hon2liv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+	);
 };
 
 module.exports = connectDB;
