d87103590a975ef6a74ef4ea88ac2c403e6ff016
VidyaSagarMehar <vidyasagark890@gmail.com>
2025-10-08 00:10:55 +0530

    sender chat and reciver chat is showing left and right

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -78,7 +78,15 @@ const Chat = () => {
 				{/* Dispaly Messages*/}
 				{messages.map((msg, index) => {
 					return (
-						<div key={index} className="chat chat-start">
+						<div
+							key={index}
+							className={
+								'chat ' +
+								(user?.data?.firstName === msg.firstName
+									? 'chat-end'
+									: 'chat-start')
+							}
+						>
 							<div className="chat-header">
 								{msg.firstName + ' ' + msg.lastName}
 								<time className="text-xs opacity-50">2 hours ago</time>
